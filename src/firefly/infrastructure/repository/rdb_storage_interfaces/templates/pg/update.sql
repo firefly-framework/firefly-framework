16acb2280b2b020f16104c636938d6b0903aabf5
jdwil <me@jdwilliams.xyz>
2020-12-28 16:37:41 -0600

    Revert "Assume all id's are uuid's. We'll need to revisit this at some point."
    This reverts commit bf163dde

--- src/firefly/infrastructure/repository/rdb_storage_interfaces/templates/pg/update.sql
+++ src/firefly/infrastructure/repository/rdb_storage_interfaces/templates/pg/update.sql
@@ -2,5 +2,5 @@
 {% import 'pg/macros.sql' as pg_macros %}
 {% block where_clause %}{{ macros.where_clause(criteria, pg_macros.attribute, pg_macros.value, ids) }}{% endblock %}
 {% block update_value %}
-    {{ v }}{%- if k in ids -%}::uuid{% endif %}{% if k == 'document' %}::json{% endif %}
+    {{ v }}{%- if k in ids and v is uuid -%}::uuid{% endif %}{% if k == 'document' %}::json{% endif %}
 {% endblock %}

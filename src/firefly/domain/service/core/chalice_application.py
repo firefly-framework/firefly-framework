4032d35f365a3a7265bdf83dc9391c2737bad56d
jdwil <me@jdwilliams.xyz>
2022-05-20 12:13:38 -0500

    this wasn't needed

--- src/firefly/domain/service/core/chalice_application.py
+++ src/firefly/domain/service/core/chalice_application.py
@@ -153,7 +153,7 @@ class ChaliceApplication(Application):
             func.__name__ = config['service'].__class__.__name__
             self.app.route(
                 path=config['route'],
-                methods=[str(config['method']).upper(), 'OPTIONS'],
+                methods=[str(config['method']).upper()],
                 name=config['service'].__class__.__name__,
                 cors=True
             )(func)

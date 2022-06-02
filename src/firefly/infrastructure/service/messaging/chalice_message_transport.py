ec37be7224f400852c0045bfbf9b1a3d87471690
jdwil <jdwilliams1982@gmail.com>
2022-06-02 09:28:45 -0500

    Added some additional test functionality to chalice message transport

--- src/firefly/infrastructure/service/messaging/chalice_message_transport.py
+++ src/firefly/infrastructure/service/messaging/chalice_message_transport.py
@@ -14,7 +14,7 @@
 
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Callable
 
 import firefly.domain as ffd
 from chalice.test import Client
@@ -23,6 +23,7 @@ from chalice.test import Client
 class ChaliceMessageTransport(ffd.MessageTransport, ffd.LoggerAware):
     _kernel: ffd.Kernel = None
     _serializer: ffd.Serializer = None
+    _test_handlers: dict[str, Callable] = {}
     _context: str = None
 
     __client: Client = None
@@ -34,8 +35,12 @@ class ChaliceMessageTransport(ffd.MessageTransport, ffd.LoggerAware):
         return self.__client
 
     def dispatch(self, event: ffd.Event) -> None:
+        if self._has_handler(str(event)):
+            return self._test_handlers[str(event)](event)
+
         if event.get_context() != self._context:
             self.info(f'dispatch(): Message {event} is for another context. Skipping.')
+
         else:
             self._client.lambda_.invoke(
                 'sqs_event',
@@ -43,11 +48,26 @@ class ChaliceMessageTransport(ffd.MessageTransport, ffd.LoggerAware):
             )
 
     def invoke(self, command: ffd.Command) -> Any:
+        if self._has_handler(str(command)):
+            return self._test_handlers[str(command)](command)
+
         return self._client.lambda_.invoke(
             str(command).split('.').pop(), self._serializer.serialize(command)
         ).payload
 
     def request(self, query: ffd.Query) -> Any:
+        if self._has_handler(str(query)):
+            return self._test_handlers[str(query)](query)
+
         return self._client.lambda_.invoke(
             str(query).split('.').pop(), self._serializer.serialize(query)
         ).payload
+
+    def register_handler(self, event: str, handler):
+        self._test_handlers[event] = handler
+
+    def reset(self):
+        self._test_handlers = {}
+
+    def _has_handler(self, msg: str):
+        return msg in self._test_handlers
